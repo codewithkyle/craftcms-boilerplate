4f996df5ab33f4a8d2eb0912ac0491e3a792c19c
Kyle Andrews <kylea@page.works>
2020-03-21 10:41:22 -0400

    Adds content builder matrix block tabs

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -22,7 +22,153 @@ return [
                         'types' => ['carouselHero', 'standardHero', 'boldHero']
                     ]
                 ],
-                'types' => [],
+                'types' => [
+                    'carouselHero' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Slides',
+                                'fields' => ['slides'],
+                            ],
+                            [
+                                'label' => 'Settings',
+                                'fields' => ['automaticTransition'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'standardHero' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Background',
+                                'fields' => ['backgroundColor', 'backgroundImage'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToAction', 'secondaryAction'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'boldHero' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Background',
+                                'fields' => ['image'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToAction', 'secondaryAction'],
+                            ]
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'blogCards' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Articles',
+                                'fields' => ['articles'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'callToActionCards' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading'],
+                            ],
+                            [
+                                'label' => 'Cards',
+                                'fields' => ['cards'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'callToActionBanner' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Background',
+                                'fields' => ['backgroundColor', 'backgroundImage'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'callToActionBanner' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Background',
+                                'fields' => ['backgroundColor', 'backgroundImage'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'richText' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                    'richTextImage' => [
+                        'tabs' => [
+                            [
+                                'label' => 'Copy',
+                                'fields' => ['heading', 'copy'],
+                            ],
+                            [
+                                'label' => 'Image',
+                                'fields' => ['image', 'imagePosition'],
+                            ],
+                            [
+                                'label' => 'Actions',
+                                'fields' => ['callToActionButton'],
+                            ],
+                        ],
+                        'defaultTabName' => 'Misc',
+                    ],
+                ],
             ],
         ],
         'form' => [
