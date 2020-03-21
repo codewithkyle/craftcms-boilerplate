5b0fd880ff118affe47cf7033c3243aa7c42b974
Kyle Andrews <kylea@page.works>
2020-03-21 09:58:42 -0400

    Organized content blocks into groups

--- config/matrixmate.php
+++ config/matrixmate.php
@@ -1,3 +1,39 @@
 <?php
 
-return [];
+return [
+    'fields' => [
+        'contentBuilder' => [
+            '*' => [
+                'groups' => [
+                    [
+                        'label' => 'Basic Content',
+                        'types' => ['richText', 'image', 'video', 'richTextImage', 'horizontalRule'],
+                    ],
+                    [
+                        'label' => 'Content Blocks',
+                        'types' => ['testimonials'],
+                    ],
+                    [
+                        'label' => 'Calls to Action Blocks',
+                        'types' => ['blogCards', 'callToActionCards', 'callToActionBanner'],
+                    ],
+                    [
+                        'label' => 'Hero Blocks',
+                        'types' => ['carouselHero', 'standardHero', 'boldHero']
+                    ]
+                ],
+                'types' => [],
+            ],
+        ],
+        'form' => [
+            '*' => [
+                'groups' => [
+                    [
+                        'label' => 'Inputs',
+                        'types' => ['singleColumn', 'twoColumns', 'threeColumns'],
+                    ],
+                ],
+            ]
+        ]
+    ],
+];
