383db21d4e286b423c021f8b739a82348166db27
Kyle Andrews <kylea@page.works>
2020-03-27 08:27:57 -0400

    Adds missing logo notice

--- cssmonster.config.js
+++ cssmonster.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	outDir: "_css",
 	sources: ["./templates", "./brixi/src"],
 	minify: true,
-	purge: true,
+	purge: false,
 	blacklist: ["./templates/frameworks"],
 	purgeCSS: {
 		content: ["./templates/**/*.twig"],
