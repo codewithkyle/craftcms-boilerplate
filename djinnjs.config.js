2f2463e8626f7a658ba8d904c33efe6d331be40d
Kyle Andrews <codingwithkyle@gmail.com>
2021-01-13 09:23:41 -0500

    Updated build tools

--- djinnjs.config.js
+++ djinnjs.config.js
@@ -1,5 +1,3 @@
 module.exports = {
 	src: ["./_compiled", "./_css"],
-	noCachePattern: /(\/webmaster\/)|(\/cpresources\/)|(index\.php)|(cachebust\.js)|(\/pwa\/)|(\.json)$/gi,
-	cachebustURL: "/pwa/cachebust.json",
 };
