245b74a4d7472e413a5024d0878ff4667f22b6e6
Kyle Andrews <codingwithkyle@gmail.com>
2021-04-23 11:37:24 -0400

    Closes #41 - updated build tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-	parserOptions: {
-		ecmaVersion: 2019,
-		sourceType: 'module',
-	},
-	rules: {
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/ban-ts-ignore': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-	},
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
--- build/bundle.js
+++ build/bundle.js
@@ -2,18 +2,24 @@ const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
 const glob = require("glob");
+const install = require("esinstall").install;
 
-const libDir = path.join(cwd, "templates", "_lib");
-if (fs.existsSync(libDir)) {
-	fs.rmdirSync(libDir, { recursive: true });
-}
-fs.mkdirSync(libDir);
+(async () => {
+	const pkg = require(path.join(cwd, "package.json"));
+	await install(pkg.bundle);
+	
+	const libDir = path.join(cwd, "templates", "_lib");
+	if (fs.existsSync(libDir)) {
+		fs.rmdirSync(libDir, { recursive: true });
+	}
+	fs.mkdirSync(libDir);
 
-const scripts = glob.sync(`${path.join(cwd, "web_modules")}/**/*.js`);
-for (let i = 0; i < scripts.length; i++){
-	const script = scripts[i];
-	const filename = script.replace(/.*[\/\\]/, "");
-	fs.renameSync(script, path.join(libDir, filename));
-}
+	const scripts = glob.sync(`${path.join(cwd, "web_modules")}/**/*.js`);
+	for (let i = 0; i < scripts.length; i++){
+		const script = scripts[i];
+		const filename = script.replace(/.*[\/\\]/, "");
+		fs.renameSync(script, path.join(libDir, filename));
+	}
 
-fs.rmdirSync(path.join(cwd, "web_modules"), {recursive: true});
+	fs.rmdirSync(path.join(cwd, "web_modules"), {recursive: true});
+})();
